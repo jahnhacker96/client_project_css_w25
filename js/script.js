268582e5fbe9eddf600bd9a2f58672a11491e6cc
Jeanette <jaahn@umich.edu>
2025-04-06 21:20:30 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+// Create and configure an iframe to display the U-M Student Life map
 const iframe = document.createElement("iframe");
 iframe.src = "https://maps.studentlife.umich.edu/";
 iframe.width = "100%";
@@ -6,4 +7,5 @@ iframe.height = "600";
 iframe.style.border = "0";
 iframe.loading = "lazy";
 
+// Add the iframe to the element with ID 'map-container'
 document.getElementById("map-container").appendChild(iframe);
